4116b583d89d829107185b0c1dbc56ef5edaf7b2
Vincze Gábor <vinczegabor1981@gmail.com>
2022-07-14 19:06:42 +0200

    starter commit

---
+++ src/test/java/com/example/jenkinsdemo/JenkinsDemoApplicationTests.java
@@ -0,0 +1,16 @@
+package com.example.jenkinsdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+@SpringBootTest
+class JenkinsDemoApplicationTests {
+
+    @Test
+    void trueTest() {
+        assertTrue(true);
+    }
+
+}
